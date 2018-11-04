99dce9440dd43b17aac14dc3df55c91474bb45ae
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-11-04 15:31:40 +0000

    all the things

--- functions/accessToken.js
+++ functions/accessToken.js
@@ -1,5 +1,7 @@
+const envs = process.env;
+
 exports.handler = function(event, context, callback) {
-    console.log(JSON.stringify(process.env));
+    console.log(JSON.stringify(process.env), envs);
     //const { CLIENT_ID, REDIRECT_URL } = process.env;
     const STATE_TOKEN = event.queryStringParameters.state;
     const AUTH_URL = `https://auth.monzo.com/?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code&state=${STATE_TOKEN}`;
