9a7866037183bc8f489a9061be9d367b0680707c
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-11-04 18:19:03 +0000

    everythings broken i bet

--- dist/functions/accessToken.js
+++ dist/functions/accessToken.js
@@ -2,7 +2,7 @@ import needle from 'needle';
 
 exports.handler = function(event, context, callback) {
     const CLIENT_ID = `${process.env.CLIENT_ID}`;
-    const CLIENT_SECRET = `helloworld`;
+    const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`;
     const REDIRECT_URL = `${process.env.REDIRECT_URL}`;
     const ACCESS_TOKEN_URL = 'https://api.monzo.com/oauth2/token';
     const CODE = event.queryStringParameters.code;
