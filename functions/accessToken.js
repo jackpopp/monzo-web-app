6c66f686236c555e2c0bbe41548656ebb5b60ee6
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-11-04 18:39:02 +0000

    wot

--- functions/accessToken.js
+++ functions/accessToken.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch/lib/index');
 
 exports.handler = function(event, context, callback) {
     const CLIENT_ID = `${process.env.CLIENT_ID}`;
