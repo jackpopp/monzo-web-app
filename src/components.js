fe36dce7e92a215e190af6bbb7fccfaa202c5a9f
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-11-05 14:05:45 +0000

    stylez

--- src/components.js
+++ src/components.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { format } from 'date-fns'
+
+import './styles.css';
 
 function formatAmount(amount) {
     return `£${((amount > 0 ? Math.abs(amount) : amount) / 100).toFixed(2)}`;
 }
 
 const App = (props) => (
-    <ul>
-        {props.transactions.map(transaction => (
-            <li>
-                <p>{transaction.description}</p>
-                <p>{formatAmount(transaction.local_amount)} - {transaction.category} - {transaction.created}</p>
-            </li>
-        ))}
-    </ul>
+    <div>
+        <header></header>
+        <ul>
+            {props.transactions.map(transaction => (
+                <li className="listing">
+                    <div className="listing__heading date">{format(transaction.created, 'dddd Do MMMM')}</div>
+                    <div className="listing__body">
+                        <div className="listing__body-left">
+                            {transaction.category}
+                            {transaction.description}
+                        </div>
+                        <div className="listing__body-right">
+                            {formatAmount(transaction.local_amount)}
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>
 );
 
 function renderApp(data) {
