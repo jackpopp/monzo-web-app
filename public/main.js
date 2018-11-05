0f0552806637b36b5a2f5cecb9c6219383914c7d
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-11-05 08:46:27 +0000

    saving token

--- public/main.js
+++ public/main.js
@@ -8,33 +8,44 @@
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
 const state = urlParams.get('state'); 
+const accessToken = storage.getItem('accessToken');
 
-if (code === null) {
+if (code === null && accessToken === null) {
     const state = new Date().getTime();
     window.location = `https://relaxed-meninsky-eaf91e.netlify.com/.netlify/functions/oauth?state=${state}`;
 }
 
-if (code !== null && state !== null) {
-    const startApp = (result) => {
-        console.log(result);
-        fetch('https://api.monzo.com/transactions?account_id=acc_00009cHB2QXoGDnGy8XFkf', { 
-            method: 'get', 
-            headers: {
-              'authorization': `Bearer ${result.access_token}`
-            }
-          }).then((res) => res.json())
-          .then((res) => {
-              console.log(res);
-            const list = res.transactions.map((transaction) => {
-                return `<li>${transaction.description} - ${Math.abs(transaction.local_amount)/100} - ${transaction.created}</li>`
-            }).join('');
-    
-            document.querySelector('.root').innerHTML = `<ul>${list}</ul>`;
-          });
-    }
-    
+function getToken() {
     const urlParams = new URLSearchParams(window.location.search);
     fetch(`https://relaxed-meninsky-eaf91e.netlify.com/.netlify/functions/accessToken?code=${code}&state=${state}`)
         .then((res) => res.json())
-        .then(startApp);
+        .then((res) => {
+            storage.setItem('accessToken', res.access_token);
+        });
+}
+
+function startApp(accessToken) {
+    fetch('https://api.monzo.com/transactions?account_id=acc_00009cHB2QXoGDnGy8XFkf', { 
+        method: 'get', 
+        headers: {
+          'authorization': `Bearer ${accesToken}`
+        }
+      }).then((res) => res.json())
+      .then((res) => {
+          console.log(res);
+        const list = res.transactions.map((transaction) => {
+            return `<li>${transaction.description} - ${Math.abs(transaction.local_amount)/100} - ${transaction.created}</li>`
+        }).join('');
+
+        document.querySelector('.root').innerHTML = `<ul>${list}</ul>`;
+      });
+}
+
+
+if ((code !== null && state !== null) || (accessToken)) {
+    if (accessToken) {
+        startApp(accessToken)
+    } else {
+        getToken().then(result => startApp(result.access_token));
+    }
 }
